a8ce9784012bb6fb11d1b4157a8aede7d968dbaa
erdosn <rafael.carrasco@flatironschool.com>
2020-08-05 16:05:48 -0500

    pushing script sample

---
+++ python_basics/scripting_sample/student.py
@@ -0,0 +1,10 @@
+# write functions to complete the following tasks
+
+def summ(a, b):
+    """write a function that returns the sum of a and b"""
+    return None
+
+
+def make_dict(k, v):
+    """write a function that returns a dictionary with k as the key and v as the value"""
+    return None
